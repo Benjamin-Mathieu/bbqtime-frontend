76dad4ad71891d1f24b0171693c711290ed2efde
Benjamin <ben88200@gmail.com>
2021-08-24 16:59:34 +0200

    Authentification user

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,13 +1,50 @@
 <template>
   <h1>SignIn</h1>
+  <form @submit.prevent="authUser()" method="post">
+    <input v-model="email" type="text" placeholder="E-mail" required />
+    <input
+      v-model="password"
+      type="password"
+      placeholder="Mot de passe"
+      required
+    />
+    <button type="submit">Connexion</button>
+  </form>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { Http } from "@capacitor-community/http";
 
 export default defineComponent({
   name: "SignIn",
-  mounted() {},
+  data() {
+    return {
+      email: "testpost@gmail.com",
+      password: "",
+    };
+  },
+  methods: {
+    authUser() {
+      console.log(this.email, this.password);
+      const options = {
+        url: "http://localhost:3000/users/login",
+        headers: {
+          Accept: "application/json",
+          "Content-type": "application/json",
+        },
+        data: {
+          email: this.email,
+          password: this.password,
+        },
+      };
+
+      const resp = Http.post(options)
+        .then((resp) => console.log(resp.data))
+        .catch((err) => console.log(err));
+      return resp;
+    },
+  },
 });
 </script>
 
