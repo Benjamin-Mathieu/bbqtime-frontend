fa358d3a13911463c3de3c85f9a85ebcba380fea
Benjamin <ben88200@gmail.com>
2021-08-27 16:59:31 +0200

    Refactoring scan

--- src/views/Scan.vue
+++ src/views/Scan.vue
@@ -1,34 +1,15 @@
 <template>
-  <h1>SCAN</h1>
-  <ion-button @click="scan()">Test scanEEEE</ion-button>
+  <h3>Veuillez scanner votre QR-code</h3>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { BarcodeScanner } from "@capacitor-community/barcode-scanner";
+import QrCode from "../services/qrcode";
 
 export default defineComponent({
   name: "Scan",
-  mounted() {},
-  methods: {
-    scan() {
-      // check or request permission
-      BarcodeScanner.checkPermission({ force: true })
-        .then((res) => console.log(res, "Permissions ok"))
-        .catch((err) => console.log(err));
-
-      BarcodeScanner.hideBackground(); // make background of WebView transparent
-
-      const result = BarcodeScanner.startScan()
-        .then((res) => {
-          console.log(res);
-          // if the result has content
-          if (result.hasContent) {
-            console.log(result.content); // log the raw scanned content
-          }
-        })
-        .catch((err) => console.log(err)); // start scanning and wait for a result
-    },
+  mounted() {
+    QrCode.scan();
   },
 });
 </script>
