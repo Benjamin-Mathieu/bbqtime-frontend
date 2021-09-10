502a04a9dfa559180e8562200e4d8bfa9255e238
Benjamin <ben88200@gmail.com>
2021-09-10 17:10:51 +0200

    Ajout boutons de redirection => mes évènements, commandes et paramètres

--- src/views/Account.vue
+++ src/views/Account.vue
@@ -33,21 +33,57 @@
           </ion-item>
         </ion-card>
       </form>
+
+      <router-link :to="{ name: 'MyEvents' }">
+        <ion-button>
+          <ion-icon size="large" :icon="calendarOutline"> </ion-icon>
+        </ion-button>
+      </router-link>
+
+      <router-link :to="{ name: 'Orders' }"
+        ><ion-button>
+          <ion-icon size="large" :icon="bagOutline"></ion-icon> </ion-button
+      ></router-link>
+
+      <router-link :to="{ name: 'Orders' }"
+        ><ion-button>
+          <ion-icon
+            size="large"
+            :icon="settingsOutline"
+          ></ion-icon> </ion-button
+      ></router-link>
     </ion-content>
   </ion-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
-import { IonCard, IonCardContent, IonItem, IonButton } from "@ionic/vue";
+import {
+  IonPage,
+  IonContent,
+  IonCard,
+  IonCardContent,
+  IonItem,
+  IonButton,
+  IonIcon,
+  IonInput,
+} from "@ionic/vue";
+import { settingsOutline, bagOutline, calendarOutline } from "ionicons/icons";
 
 export default defineComponent({
   name: "Account",
+  setup() {
+    return { settingsOutline, bagOutline, calendarOutline };
+  },
   components: {
+    IonPage,
+    IonContent,
     IonCard,
     IonCardContent,
     IonItem,
     IonButton,
+    IonIcon,
+    IonInput,
   },
   methods: {
     updateProfile() {
