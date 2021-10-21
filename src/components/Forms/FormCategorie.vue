fcccb839255113bceb867a11abd81aeddbb39e0f
Benjamin <ben88200@gmail.com>
2021-10-21 11:20:07 +0200

    Suppression upload image

--- src/components/Forms/FormCategorie.vue
+++ src/components/Forms/FormCategorie.vue
@@ -19,26 +19,6 @@
             <ion-label position="floating">Nom de la catégorie</ion-label>
             <ion-input type="text" v-model="libelle" required></ion-input>
           </ion-item>
-          <ion-item lines="none">
-            <ion-thumbnail v-if="this.file" slot="end">
-              <ion-img :src="this.img" alt="img"></ion-img>
-            </ion-thumbnail>
-            <input
-              style="display: none"
-              type="file"
-              name="image"
-              @change="pickImage"
-              ref="fileInput"
-            />
-
-            <ion-button
-              size="small"
-              fill="clear"
-              @click="$refs.fileInput.click()"
-            >
-              Poster une image
-            </ion-button>
-          </ion-item>
         </ion-card-content>
         <ion-item>
           <ion-button type="submit" size="small">Ajouter</ion-button>
@@ -82,21 +62,11 @@ export default defineComponent({
   data() {
     return {
       libelle: "",
-      file: null,
-      img: null,
     };
   },
   methods: {
-    pickImage(selected) {
-      this.file = selected.target.files[0];
-      this.img = URL.createObjectURL(this.file);
-    },
-    addCategorie() {
-      const data = {
-        libelle: this.libelle,
-        file: this.file,
-      };
-      this.$store.dispatch("postCategorie", data);
+    async addCategorie() {
+      await this.$store.dispatch("postCategorie", this.libelle);
       this.libelle = "";
     },
     async closeModal() {
