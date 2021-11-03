2c4d805c94a03887e5dd19fa265f4c737a74e5c9
Benjamin <ben88200@gmail.com>
2021-11-03 17:02:29 +0100

    Modification style vue "ResetPassword"

--- src/views/ResetPassword.vue
+++ src/views/ResetPassword.vue
@@ -1,72 +1,85 @@
 <template>
   <ion-page>
     <Header></Header>
-    <Sub></Sub>
+    <Sub title="Réinitialisation mot de passe"></Sub>
     <ion-content>
-      <ion-card>
-        <ion-card-subtitle>
-          Réinitialisation du mot de passe
-        </ion-card-subtitle>
-        <ion-card-content>
-          <form
-            v-if="showFormSendCode"
-            @submit.prevent="sendCode()"
-            method="post"
-          >
-            <ion-input
-              v-model="email"
-              type="email"
-              placeholder="Saisir votre adresse mail"
-              required
-            ></ion-input>
-            <ion-button type="submit" size="small">Envoyer le code</ion-button>
-          </form>
+      <div id="container">
+        <ion-card>
+          <ion-card-header>
+            <ion-card-title color="primary"
+              >Mot de passe oublié ?</ion-card-title
+            >
+          </ion-card-header>
+          <ion-card-content>
+            <form
+              v-if="showFormSendCode"
+              @submit.prevent="sendCode()"
+              method="post"
+            >
+              <ion-item>
+                <ion-input
+                  v-model="email"
+                  type="email"
+                  placeholder="Saisir votre adresse mail"
+                  required
+                ></ion-input>
+              </ion-item>
 
-          <form
-            v-if="showFormVerifCode"
-            @submit.prevent="verifCode()"
-            method="post"
-          >
-            <ion-input
-              v-model.number="code"
-              type="number"
-              placeholder="Saisir code"
-              required
-            ></ion-input>
-            <ion-button type="submit" size="small">Valider</ion-button>
-          </form>
+              <div class="submit-buttons">
+                <ion-button type="submit" size="small">Continuer</ion-button>
+              </div>
+            </form>
 
-          <form
-            v-if="showFormResetPassword"
-            @submit.prevent="resetPassword()"
-            method="post"
-          >
-            <ion-item>
-              <ion-label position="floating">Nouveau mot de passe</ion-label>
+            <form
+              v-if="showFormVerifCode"
+              @submit.prevent="verifCode()"
+              method="post"
+            >
               <ion-input
-                v-model="password"
-                type="password"
-                placeholder="Saisir nouveau mot de passe"
+                v-model.number="code"
+                type="number"
+                inputmode="numeric"
+                placeholder="Saisir code"
                 required
               ></ion-input>
-            </ion-item>
 
-            <ion-item>
-              <ion-label position="floating">Saisir à nouveau</ion-label>
-              <ion-input
-                v-model="checkpwd"
-                type="password"
-                placeholder="Saisir nouveau mot de passe"
-                required
-              ></ion-input>
-            </ion-item>
+              <div class="submit-buttons">
+                <ion-button type="submit" size="small">Valider</ion-button>
+              </div>
+            </form>
 
-            <ion-button type="submit" size="small">
-              Changer mot de passe
-            </ion-button>
-          </form>
-        </ion-card-content>
-      </ion-card>
+            <form
+              v-if="showFormResetPassword"
+              @submit.prevent="resetPassword()"
+              method="post"
+            >
+              <ion-item>
+                <ion-label position="floating">Nouveau mot de passe</ion-label>
+                <ion-input
+                  v-model="password"
+                  type="password"
+                  placeholder="Saisir nouveau mot de passe"
+                  required
+                ></ion-input>
+              </ion-item>
+
+              <ion-item>
+                <ion-label position="floating">Saisir à nouveau</ion-label>
+                <ion-input
+                  v-model="checkpwd"
+                  type="password"
+                  placeholder="Saisir nouveau mot de passe"
+                  required
+                ></ion-input>
+              </ion-item>
+
+              <div class="submit-buttons">
+                <ion-button type="submit" size="small"> Changer </ion-button>
+              </div>
+            </form>
+          </ion-card-content>
+        </ion-card>
+      </div>
     </ion-content>
     <Footer></Footer>
   </ion-page>
@@ -78,7 +91,8 @@ import {
   IonPage,
   IonContent,
   IonCard,
-  IonCardSubtitle,
+  IonCardTitle,
+  IonCardHeader,
   IonCardContent,
   IonInput,
   IonButton,
@@ -91,11 +105,13 @@ import Footer from "../components/Footer.vue";
 import popup from "../components/ToastController";
 
 export default defineComponent({
+  name: "ResetPassword",
   components: {
     IonPage,
     IonContent,
     IonCard,
-    IonCardSubtitle,
+    IonCardTitle,
+    IonCardHeader,
     IonCardContent,
     IonInput,
     IonButton,
@@ -105,10 +121,10 @@ export default defineComponent({
     Sub,
     Footer,
   },
-  name: "ResetPassword",
+
   data() {
     return {
-      email: "ben-88200@hotmail.fr",
+      email: "",
       code: "",
       password: "",
       checkpwd: "",
@@ -117,6 +133,7 @@ export default defineComponent({
       showFormResetPassword: false,
     };
   },
+
   methods: {
     async sendCode() {
       this.$store.commit("setResetPassword", { email: this.email });
@@ -126,20 +143,21 @@ export default defineComponent({
         this.showFormVerifCode = true;
       }
     },
+
     async verifCode() {
       const success = await this.$store.dispatch("verifCode", this.code);
       if (success) {
         this.showFormVerifCode = false;
         this.showFormResetPassword = true;
       } else {
-        popup.error("Code incorrect");
+        popup.warning("Code incorrect");
         this.code = "";
       }
     },
 
     async resetPassword() {
       if (this.password !== this.checkpwd) {
-        popup.error("Les mots de passes saisies ne correspondent pas");
+        popup.warning("Les mots de passes saisies ne correspondent pas");
       } else {
         await this.$store.dispatch("resetPassword", this.checkpwd);
       }
@@ -149,4 +167,20 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
+ion-card {
+  --padding: 1em;
+}
+
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+
+  .submit-buttons {
+    margin-top: 1em;
+  }
+}
 </style>
