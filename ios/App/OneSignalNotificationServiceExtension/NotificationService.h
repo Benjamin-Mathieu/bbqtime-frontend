639843982262d9801fa35bef04d15cbb12e616be
Benjamin <ben88200@gmail.com>
2021-11-15 15:00:19 +0100

    Modification config pour iOS

---
+++ ios/App/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by MAC MINI LTG SERVICES on 12/11/2021.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
