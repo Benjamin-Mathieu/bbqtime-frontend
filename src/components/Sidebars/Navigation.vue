2015039dcaa87ab613cb3f8a522374c9ab4b0440
Benjamin <ben88200@gmail.com>
2021-12-01 14:22:29 +0100

    Modification style sous-menu événement

--- src/components/Sidebars/Navigation.vue
+++ src/components/Sidebars/Navigation.vue
@@ -2,77 +2,77 @@
   <ion-menu color="primary" side="start" menu-id="nav" content-id="main">
     <ion-content @click="closeMenu()" class="menu">
       <div id="container">
-      <ion-list-header>
-        <router-link :to="{ name: 'Home' }">
-          <img src="../../../public/assets/logo1.png" alt="logo-bbqtime" />
-        </router-link>
-      </ion-list-header>
-      <ion-list v-if="!this.$store.state.auth.userIsLoggedIn" lines="none">
-        <router-link class="links" :to="{ name: 'Events' }">
+        <ion-list-header>
+          <router-link :to="{ name: 'Home' }">
+            <img src="../../../public/assets/logo1.png" alt="logo-bbqtime" />
+          </router-link>
+        </ion-list-header>
+        <ion-list v-if="!this.$store.state.auth.userIsLoggedIn" lines="none">
+          <router-link class="links" :to="{ name: 'Events' }">
+            <ion-item>
+              <p>Evènements</p>
+            </ion-item>
+          </router-link>
+          <router-link class="links" :to="{ name: 'Shop' }">
+            <ion-item>
+              <p>Panier</p>
+            </ion-item>
+          </router-link>
+          <router-link class="links" :to="{ name: 'SignIn' }">
+            <ion-item>
+              <p>Connexion</p>
+            </ion-item>
+          </router-link>
+        </ion-list>
+        <ion-list v-else lines="none">
           <ion-item>
-            <p>Evènements</p>
+            <p @click.stop="show()">Evènements</p>
           </ion-item>
-        </router-link>
-        <router-link class="links" :to="{ name: 'Shop' }">
-          <ion-item>
-            <p>Panier</p>
+          <ion-item v-if="isClicked">
+            <ul>
+              <li>
+                <router-link class="links" :to="{ name: 'Events' }">
+                  Publiques
+                </router-link>
+              </li>
+              <li>
+                <router-link class="links" :to="{ name: 'MyEvents' }">
+                  Mes évènements
+                </router-link>
+              </li>
+            </ul>
           </ion-item>
-        </router-link>
-        <router-link class="links" :to="{ name: 'SignIn' }">
-          <ion-item>
-            <p>Connexion</p>
-          </ion-item>
-        </router-link>
-      </ion-list>
-      <ion-list v-else lines="none">
-        <ion-item>
-          <p @click.stop="show()">Evènements</p>
-        </ion-item>
-        <ion-item v-if="isClicked">
-          <ul>
-            <li>
-              <router-link class="links" :to="{ name: 'Events' }">
-                Publiques
-              </router-link>
-            </li>
-            <li>
-              <router-link class="links" :to="{ name: 'MyEvents' }">
-                Mes évènements
-              </router-link>
-            </li>
-          </ul>
-        </ion-item>
-        <router-link class="links" :to="{ name: 'Orders' }">
-          <ion-item>
-            <p>Commandes</p>
-          </ion-item>
-        </router-link>
-        <router-link class="links" :to="{ name: 'Shop' }">
-          <ion-item>
-            <p>Panier</p>
-          </ion-item>
-        </router-link>
-        <router-link class="links" :to="{ name: 'AddEvent' }">
-          <ion-item>
-            <p>Créer un évènement</p>
-          </ion-item>
-        </router-link>
-        <router-link class="links" :to="{ name: 'Scan' }">
-          <ion-item>
-            <p>Scan</p>
-          </ion-item>
-        </router-link>
-        <router-link class="links" :to="{ name: 'Account' }">
-          <ion-item>
-            <p>Mon compte</p>
-          </ion-item>
-        </router-link>
-        <div class="logout-button">
-          <ion-button size="medium" @click="logoutUser()">
-            Déconnexion
-          </ion-button>
-        </div>
-      </ion-list>
+          <router-link class="links" :to="{ name: 'Orders' }">
+            <ion-item>
+              <p>Commandes</p>
+            </ion-item>
+          </router-link>
+          <router-link class="links" :to="{ name: 'Shop' }">
+            <ion-item>
+              <p>Panier</p>
+            </ion-item>
+          </router-link>
+          <router-link class="links" :to="{ name: 'AddEvent' }">
+            <ion-item>
+              <p>Créer un évènement</p>
+            </ion-item>
+          </router-link>
+          <router-link class="links" :to="{ name: 'Scan' }">
+            <ion-item>
+              <p>Scan</p>
+            </ion-item>
+          </router-link>
+          <router-link class="links" :to="{ name: 'Account' }">
+            <ion-item>
+              <p>Mon compte</p>
+            </ion-item>
+          </router-link>
+          <div class="logout-button">
+            <ion-button size="medium" @click="logoutUser()">
+              Déconnexion
+            </ion-button>
+          </div>
+        </ion-list>
       </div>
     </ion-content>
   </ion-menu>
@@ -142,6 +142,11 @@ ion-item {
 ion-list {
   padding-bottom: 0px;
   padding-top: 0px;
+
+  ul li {
+    list-style-type: none;
+    margin: 1em 0;
+  }
 
   .links {
     text-decoration: none;
